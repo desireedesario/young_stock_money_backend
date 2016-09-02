055d50150eeb64458e2aeceb9193decd52af845b
Desi Desario <desireedesario@gmail.com>
2016-09-02 11:38:18 -0700

    added database

--- young_stock_money_api/app.js
+++ young_stock_money_api/app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('./config/database');
 var env = require('dotenv').config();
 
 var routes = require('./routes/index');
