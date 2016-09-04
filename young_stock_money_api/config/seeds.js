5b5466f8376b7dcbb0c8ab83f4da0ada0f15e29c
Desi Desario <desireedesario@gmail.com>
2016-09-04 13:55:28 -0700

    added seeds

--- young_stock_money_api/config/seeds.js
+++ young_stock_money_api/config/seeds.js
@@ -1,1 +1,66 @@
 var mongoose = require('../config/database');
+
+var User = require ('../models/user');
+
+var users = [
+  {
+    email        : 'email@email.com',
+    username     : 'user123',
+    name         : 'Fake User',
+    cash         : 50000
+  }
+]
+
+var PortfolioItem = require ('../models/PortfolioItem');
+
+var portfolioItems = [
+  {
+    stock          : 1,
+    shares         : 4,
+    purchasePrice  : 236.76
+  }
+]
+
+var Stock = require ('../models/stock');
+
+var stocks = [
+  {
+    fullName      : "Activision Blizzard, Inc.",
+    ticker        : 'ATVI',
+    currentPrice  : 42.81
+  }
+]
+
+User.remove({}, function  (err){
+  if(err) console.log(err);
+  User.create(users, function(err, users){
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Database seeded with " + users.length + " users.");
+    }
+  });
+});
+
+PortfolioItem.remove({}, function  (err){
+  if(err) console.log(err);
+  PortfolioItem.create(portfolioItems, function(err, portfolioItems){
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Database seeded with " + portfolioItems.length + " portfolio items.");
+    }
+  });
+});
+
+Stock.remove({}, function  (err){
+  if(err) console.log(err);
+  Stock.create(stocks, function(err, stocks){
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Database seeded with " + stocks.length + " stocks.");
+    }
+    process.exit();
+  });
+});
